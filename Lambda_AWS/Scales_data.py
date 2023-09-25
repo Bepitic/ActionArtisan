a45426344238dc526cf2248198c6406cc2ebdd4f
Bepitic <bepitic@gmail.com>
2023-09-25 16:44:15 +0200

    add data_scalers, logging and some documentation

---
+++ Lambda_AWS/Scales_data.py
@@ -0,0 +1,36 @@
+
+achievement_scale = [
+        "Catastrophic Failure",
+        "Significant Failure",
+        "Major Struggle",
+        "Partial Failure",
+        "Barely Missed It",
+        "Barely Got It",
+        "Lucky Success",
+        "Solid Success",
+        "Great Success",
+        "Exceptional Achievement"
+      ]
+
+difficulty_scale = [
+      "Extremely Easy",
+      "Very Easy",
+      "Quite Easy",
+      "Easy",
+      "Moderately Easy",
+      "Fairly Easy",
+      "Slightly Easy",
+      "Somewhat Easy",
+      "Neutral/Moderate",
+      "Somewhat Challenging",
+      "Slightly Challenging",
+      "Fairly Challenging",
+      "Moderately Challenging",
+      "Challenging",
+      "Quite Challenging",
+      "Difficult",
+      "Very Difficult",
+      "Extremely Difficult",
+      "Nearly Impossible",
+      "Impossible"
+    ]
